7ee181b3cdb83d3df0940b9987717012c1ead819
olehstrokan <oleh@wattstor.com>
2021-11-12 21:53:23 +0100

    Added firebase config. First get request to firebase database

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import db from './firebase'
+import { collection, onSnapshot } from 'firebase/firestore'
 function App() {
+  const [users, setUsers] = React.useState<any>();
+
+  React.useEffect(() => {
+      onSnapshot(collection(db, 'users'),(snapshot) => {
+        setUsers(snapshot.docs.map((doc) => doc.data()))
+      })
+  },[])
+
+  if (!users) {
+    return <div>...loading</div>
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+     {users.map((user: any) => <div key={user.id}>{user.title}</div>)}
+   </div>
   );
 }
 
---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+import { initializeApp }  from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDE3NVKYD0DvQ_HlnUnTRHzyxxzaQBnjnI",
+  authDomain: "information-system-vspj.firebaseapp.com",
+  projectId: "information-system-vspj",
+  storageBucket: "information-system-vspj.appspot.com",
+  messagingSenderId: "184939012723",
+  appId: "1:184939012723:web:3b2044d526b9e7b63dc006",
+  measurementId: "G-2WW65QGQXK"
+};
+
+export const app = initializeApp(firebaseConfig)
+
+export default getFirestore();
+
